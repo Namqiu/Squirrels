859a1f0649cf6de75f2c445cbc0fe3148e70e012
Chennan <cq2218@columbia.edu>
2019-12-05 02:59:52 +0000

    add three views(sightings,update,add)

---
+++ Tracker/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('sightings/', views.index, name='index'),
+    path('sightings/add/', views.add,name="add"),
+    path('sightings/<unique_squirrel_id>/', views.detail,name="detail"),
+]
--- Tracker/views.py
+++ Tracker/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import Http404
+from .models import Squirrel
+from .forms import PetForm
 
-# Create your views here.
+def index(request):
+    squirrel_list = Squirrel.objects.all()
+    context = {'squirrel_list': squirrel_list,}
+    return render(request,'Tracker/index.html',context)
+
+
+def detail(request,unique_squirrel_id):
+    try:
+        squirrel = Squirrel.objects.get(unique_squirrel_id= unique_squirrel_id)
+    except Squirrel.DoesNotExist:
+        raise Http404("Squirrel does not exist")
+    if request.method == 'POST':
+        form = PetForm(request.POST, instance = squirrel)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PetForm(instance=squirrel)
+
+    context = {'form':form,}
+    return render(request, 'Tracker/detail.html',context)
+
+def add(request):
+    if request.method == 'POST':
+        form = PetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/squirreltracker/sightings/')
+
+    else:
+        form = PetForm()
+    context = {'form':form,}
+    return render(request, 'Tracker/detail.html',context)
+
