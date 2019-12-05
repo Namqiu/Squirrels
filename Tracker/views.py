7ce3ef16d36d1e9a1ac1500352b0ca1c7ef96ee1
Jingying Wu <jw3865@columbia.edu>
2019-12-05 01:15:33 +0000

    add view for map

--- Tracker/views.py
+++ Tracker/views.py
@@ -1,3 +1,12 @@
+import random
+
 from django.shortcuts import render
+from .models import Squirrel
+
+def map(request):
+    sightings = Squirrel.objects.all().order_by('?')[:50]
+    context = {'sightings':sightings}
+    return render(request,'Tracker/map.html',context)
+
 
 # Create your views here.
