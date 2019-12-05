c993a90814a0e4220180514bdf03cbabbd8df4ca
Chennan <cq2218@columbia.edu>
2019-12-05 04:32:21 +0000

    add the general_stats view

--- Tracker/urls.py
+++ Tracker/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('sightings/', views.index, name='index'),
+    path('sightings/stats/', views.general_stats, name = "general_stats"),
     path('sightings/add/', views.add,name="add"),
     path('sightings/<unique_squirrel_id>/', views.detail,name="detail"),
     path('map/',views.map,name='map'),
--- Tracker/views.py
+++ Tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import Http404
+from django.db.models import Count,Sum,Avg
 from .models import Squirrel
 from .forms import PetForm
 
@@ -42,4 +43,19 @@ def map(request):
     context = {'sightings':sightings}
     return render(request,'Tracker/map.html',context)
 
+def general_stats(request):
+    squirrel_list = Squirrel.objects.all()
+    shift_count = squirrel_list.values('shift').annotate(count=Count('unique_squirrel_id')).values('shift','count')
+    age_count = squirrel_list.values('age').annotate(count=Count('unique_squirrel_id')).values('age','count')
+    primary_fur_color_count = squirrel_list.values('primary_fur_color').annotate(count=Count('unique_squirrel_id')).values('primary_fur_color','count')
+    location_count = squirrel_list.values('location').annotate(count=Count('unique_squirrel_id')).values('location','count')
+    running_count = squirrel_list.values('running').annotate(count=Count('unique_squirrel_id')).values('running','count')
+    context = {
+        "shift_count":shift_count,
+        "age_count":age_count,
+        "primary_fur_color_count":primary_fur_color_count,
+        "location_count":location_count,
+        "running_count":running_count,
+    }
+    return render(request, 'Tracker/stats.html',context)
 
